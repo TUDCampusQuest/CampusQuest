8618674bb8a072385ff674c63273b5b60c2749a7
BlastPowa <97843706+BlastPowa@users.noreply.github.com>
2026-01-13 20:07:32 +0000

    deleted files

--- .next/dev/server/chunks/_next-internal_server_app_api_trails_route_actions_50825e2d.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/trails/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_trails_route_actions_50825e2d.js.map
--- .next/dev/static/chunks/src_app_page_bcef28c5.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_1815bf8a._.js",
-  "static/chunks/src_app_04370454._.js"
-],
-    source: "dynamic"
-});
