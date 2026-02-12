f90e4ff2a400ade2ed738cf34282ba29cbdc5b25
Doz1E <chidozieonyejelem1@gmail.com>
2026-02-12 03:49:38 +0000

    Update locations.js

--- src/app/data/locations.js
+++ src/app/data/locations.js
@@ -1,12 +1,14 @@
-const locations = [
-  {
-    id: "FBL",
-    name: "F Block",
-    description: "Lecture halls, classrooms, library...",
-    floor: 0,
-    accessibility: "Lift",
-    indoorInstructions: "Library is straight ahead...",
-    lat: 53.406, 
-    lng: -6.379
-  }
+export const locations = [
+    {
+        id: "FBL",
+        name: "F Block",
+        description: "Lecture halls, classrooms, library...",
+        floor: 0,
+        accessibility: "Lift",
+        indoorInstructions: "Library is straight ahead...",
+        lat: 53.406,
+        lng: -6.379
+    }
 ];
+
+export default locations;
