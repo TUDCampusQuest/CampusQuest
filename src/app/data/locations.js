5b5b1d62dcd38a600cd48412db14b35520115617
Doz1E <chidozieonyejelem1@gmail.com>
2026-03-11 23:21:34 +0000

    updated images on building pages

--- src/app/data/locations.js
+++ src/app/data/locations.js
@@ -1,14 +1,15 @@
 /**
  * Campus Quest - TU Dublin Blanchardstown Locations
- * Converted to Mapbox Format [Longitude, Latitude]
- * All wording and structure preserved from original JSON.
+ * Coordinates in Mapbox format [Longitude, Latitude]
  */
+
+const S3 = "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos";
 
 export const locations = [
     {
         "id": "CAFE",
         "name": "LINC Building",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/lincbuilding.jpg`,
         "coordinates": [-6.379799, 53.406355],
         "description": "Cafe shop, admin offices.",
         "floors": ["Ground", "First"]
@@ -16,7 +17,7 @@ export const locations = [
     {
         "id": "AG-BLOCK",
         "name": "New Building",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/agblock.jpg`,
         "coordinates": [-6.379109, 53.404622],
         "description": "Classrooms, lecture halls, seating areas, meeting rooms.",
         "floors": ["Ground", "First"]
@@ -24,7 +25,7 @@ export const locations = [
     {
         "id": "A-BLOCK",
         "name": "Computer Labs",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/ablock.jpg`,
         "coordinates": [-6.376366, 53.406213],
         "description": "Classrooms, lecture halls, seating areas.",
         "floors": ["Ground", "First"]
@@ -32,7 +33,7 @@ export const locations = [
     {
         "id": "C-BLOCK",
         "name": "Main Building",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/cblock.jpg`,
         "coordinates": [-6.378489, 53.405434],
         "description": "Canteen, SU, sports hall, corner shop.",
         "floors": ["Ground", "First"]
@@ -40,7 +41,7 @@ export const locations = [
     {
         "id": "D-BLOCK",
         "name": "Engineering and Childcare",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/dblock.jpg`,
         "coordinates": [-6.377446, 53.405724],
         "description": "Lecture halls, engineering rooms, childcare rooms.",
         "floors": ["Ground"]
@@ -48,7 +49,7 @@ export const locations = [
     {
         "id": "E-BLOCK",
         "name": "Networking Building",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/eblock.jpg`,
         "coordinates": [-6.377734, 53.405241],
         "description": "Classrooms, lecture halls, networking rooms.",
         "floors": ["Ground", "First"]
@@ -56,7 +57,7 @@ export const locations = [
     {
         "id": "F-BLOCK",
         "name": "Campus Library & Information Office",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/fblock.jpg`,
         "coordinates": [-6.378422, 53.404746],
         "description": "Library, classrooms, lecture halls, security.",
         "floors": ["Ground", "First"]
@@ -64,7 +65,7 @@ export const locations = [
     {
         "id": "S-BLOCK",
         "name": "Sports Building",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/sblock.jpg`,
         "coordinates": [-6.381347, 53.405884],
         "description": "Gym and training facilities, classrooms.",
         "floors": ["Ground"]
@@ -72,7 +73,7 @@ export const locations = [
     {
         "id": "CONNECT",
         "name": "Connect Building",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/connectbuilding.jpg`,
         "coordinates": [-6.379305, 53.404810],
         "description": "Student services desk.",
         "floors": ["Ground", "First"]
@@ -80,7 +81,7 @@ export const locations = [
     {
         "id": "PARKING",
         "name": "Car Park",
-        "image": "https://campusquesttud.s3.eu-west-1.amazonaws.com/photos/tud.jpeg",
+        "image": `${S3}/tud.jpeg`,
         "coordinates": [-6.380530, 53.404767],
         "description": "Main visitor and staff campus parking.",
         "floors": ["N/A"]
